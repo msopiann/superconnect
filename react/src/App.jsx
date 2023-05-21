20371021700eaabd1691c680fa7d26ff549f341b
msopiann <muhammadsopian452@gmail.com>
2023-05-21 22:38:31 +0700

    create auth: login, register, forgot, reset.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+import AuthLayout from "./layouts/AuthLayout";
+import GuestLayout from "./layouts/GuestLayout";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="bg-slate-100 min-h-screen">
+            <Routes>
+                <Route element={<AuthLayout />}>
+                    <Route path="/" element={<Home />}></Route>
+                </Route>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <Route element={<GuestLayout />}>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                    <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+                    <Route path="/password-reset/:token" element={<ResetPassword />}></Route>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
